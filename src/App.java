ef8f6142775071f451a9844bf29afd5e85aa099f
ashlee1231 <ashleekirch11@gmail.com>
2026-01-14 18:30:06 +0000

    commit

--- src/App.java
+++ src/App.java
@@ -14,12 +14,16 @@ public class App {
         // -------------------------------------------------------------
         // TODO 1: Create a Scanner for user input
         // -------------------------------------------------------------
-
+        Scanner scanner = new Scanner(System.in);
 
         // -------------------------------------------------------------
         // TODO 2: Give information about your program
         //         Ask the user about their goals (if applicable)
         // -------------------------------------------------------------
+        System.out.println("This program will help you analyze your weekly screentime.");
+        System.out.println("Please enter your weekly goal time to be under for the week.");
+        System.out.print("Weekly goal in hours: ");
+        double goal = scanner.nextDouble();
 
 
         // -------------------------------------------------------------
@@ -27,7 +31,8 @@ public class App {
         //         Use an appropriate data type (int or double)
         //         Name the array weekData
         // -------------------------------------------------------------
-
+        double[] weekData = new double[7];
+        String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 
         // -------------------------------------------------------------
         // TODO 4: Use a for loop to collect data for each day of the week
@@ -38,13 +43,22 @@ public class App {
         //         - Use a while loop to prevent negative values
         //         - Re-prompt if the value is invalid
         // -------------------------------------------------------------
+        for (int i = 0; i < weekData.length; i++) {
+            System.out.print("Enter screentime hours for " + days[i] + ": ");
+            double input = scanner.nextDouble();
+            while (input < 0) {
+                System.out.print("Invalid input. Please enter a non-negative value for " + days[i] + ": ");
+                input = scanner.nextDouble();
+            }
+            weekData[i] = input;
+        }
 
 
         // -------------------------------------------------------------
         // TODO 5: Create a WeeklyData object
         //         Pass the weekData array into the constructor
         // -------------------------------------------------------------
-
+        WeeklyData weeklyData = new WeeklyData(weekData);
 
         // -------------------------------------------------------------
         // TODO 6: Display the results of the analysis
@@ -56,12 +70,17 @@ public class App {
         //
         //         Use clear labels and formatted output if needed
         // -------------------------------------------------------------
+        System.out.println("Total amount of screentime this week: " + weeklyData.getTotal() + " hours");
+        System.out.println("Average daily screentime: " + weeklyData.getAverage() + " hours");
+        System.out.println("Maximum screentime in a day: " + weeklyData.getMax() + " hours");
+        System.out.println("Minimum screentime in a day: " + weeklyData.getMin() + " hours");
 
 
         // -------------------------------------------------------------
         // TODO 7: Display the full week of data
         //         Use the toString() method from WeeklyData
         // -------------------------------------------------------------
+        weeklyData.toString();
 
 
         // -------------------------------------------------------------
@@ -70,6 +89,12 @@ public class App {
         //         --> "You were very hydrated this week!"
         //         --> etc.
         // -------------------------------------------------------------
+
+        if (weeklyData.getTotal() > goal) {
+            System.out.println("You spent a lot of time on screens this week! Consider taking breaks.");
+        } else {
+            System.out.println("Great job keeping your screentime under control this week!");
+        }
 
 
     }
---
+++ src/WeeklyData.java
@@ -0,0 +1,102 @@
+/**
+ * The WeeklyData class stores and analyzes a week's worth of numeric data.
+ * This could represent steps taken, hours of sleep, money spent, screen time,
+ * or any other measurable daily value.
+ */
+public class WeeklyData {
+
+    // Instance Variables
+    private double[] hoursList;
+    private double goal;
+
+    /**
+     * Constructs a WeeklyData object by taking in an array of values
+     * and making a deep copy (element-by-element) into the internal array.
+     *
+     * @param input an array representing 7 days of data
+     */
+    public WeeklyData(double[] input) {
+        this.hoursList = new double[input.length];
+        for (int i = 0; i < input.length; i++) {
+            this.hoursList[i] = input[i];
+        } 
+    }
+
+    /**
+     * Calculates and returns the total of all values in the week.
+     *
+     * @return the sum of all values in the data array
+     */
+    public double getTotal() {
+        double sum = 0;
+        for (double hour : hoursList) {
+            sum += hour;
+        }
+        return sum;
+    }
+
+    /**
+     * Calculates and returns the average value for the week.
+     *
+     * @return the average of the values in the array,
+     *         or 0.0 if the array is empty
+     */
+    public double getAverage() {
+        if (hoursList.length == 0) {
+            return 0.0;
+        }
+        else {
+            return getTotal() / hoursList.length;
+        }
+    }
+
+    /**
+     * Finds and returns the highest value in the data array.
+     *
+     * @return the maximum value
+     */
+    public double getMax() {
+        double max = hoursList[0];
+        for (int i = 1; i < hoursList.length; i++) {
+            if (hoursList[i] > max) {
+                max = hoursList[i];
+            }
+        }
+        return max;
+    }
+
+    /**
+     * Finds and returns the lowest value in the data array.
+     *
+     * @return the minimum value
+     */
+    public double getMin() {
+        double min = hoursList[0];
+        for (int i = 1; i < hoursList.length; i++) {
+            if (hoursList[i] < min) {
+                min = hoursList[i];
+            }
+        }
+        return min;
+    }
+
+    /**
+     * Returns a formatted, multi-line String showing each day's data.
+     *
+     * Example:
+     * Day 1: 4500
+     * Day 2: 6200
+     * Day 3: 5100
+     *
+     * @return a formatted String representing the week's data
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < hoursList.length; i++) {
+            sb.append("Day " + (i + 1) + ": " + hoursList[i] + "\n");
+        }
+        String finalString = sb.toString();
+        return finalString;
+    }
+}
